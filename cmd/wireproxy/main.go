84f4795ea76f9c3168a61e478d0fe0e5c3238308
Artem Russkikh <rusartx@gmail.com>
2026-08-27 22:48:25 +0400

    Bump to v1.0.18

--- cmd/wireproxy/main.go
+++ cmd/wireproxy/main.go
@@ -28,7 +28,7 @@ var default_config_paths = []string {
     os.Getenv("HOME")+"/.config/wireproxy.conf",
 }
 
-var version = "1.0.17-dev"
+var version = "1.0.18-dev"
 
 func panicIfError(err error) {
 	if err != nil {
